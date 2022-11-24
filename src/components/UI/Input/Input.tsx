4f8fc7d4c15e7db32b3c712cfdfc9547ffbc354e
Oleg Somov <gog0125@mail.ru>
2022-11-24 21:38:16 +0300

    add new styles for input

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -4,15 +4,19 @@ import React from 'react';
 import styles from './Input.module.scss';
 
 interface IProps {
-  change: () => void;
+  change: (e: React.ChangeEvent<HTMLInputElement>) => void;
   value: string;
   placeholder?: string;
-  size: 'long' | 'medium' | 'short';
+  size: 'long' | 'medium' | 'short' | 'full';
 }
 
 export const Input: React.FC<IProps> = ({ change, value, placeholder, size }) => (
   <input
-    className={clsx(styles.input, { [styles.long]: size === 'long' })}
+    className={clsx(styles.input, {
+      [styles.long]: size === 'long',
+      [styles.medium]: size === 'medium',
+      [styles.short]: size === 'short'
+    })}
     placeholder={placeholder}
     onChange={change}
     value={value}
